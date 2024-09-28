608f98a0df0b6fabb8026814f85b761a3e0726fc
iVANECHKA <akk.company2014@gmail.com>
2024-09-28 03:38:02 +0300

    Фикс фона

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Layout = ({children}) => {
   return (
-    <div className='bg-zinc-900 h-screen py-10'>{children}</div>
+    <div className='bg-zinc-900 min-h-screen py-10'>{children}</div>
   )
 }
 
