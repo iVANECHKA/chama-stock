e1eb370e39221ab45317cac78142b1c332d2a3bd
iVANECHKA <akk.company2014@gmail.com>
2024-10-01 02:35:41 +0300

    Функционал поиска

--- src/components/screens/Home/Home.jsx
+++ src/components/screens/Home/Home.jsx
@@ -9,39 +9,54 @@ const Home = () => {
 
 
     const [products, setProducts] = useState([])
+    const [searchItem, setSearchItem] = useState('')
+    const [filteredProducts, setFilteredProducts] = useState([])
 
     // Собираем данные
     async function fetchProducts() {
         const { data } = await supabase.from('products').select('*')
         setProducts(data)
+        setFilteredProducts(data)
     }
+
+
 
     useEffect(() => {
         fetchProducts()
     }, [])
 
-    console.log(products);
+    const handleInputChange = (e) => { 
+        const searchTerm = e.target.value;
+        setSearchItem(searchTerm)
+    
+        // filter the items using the apiUsers state
+        const filteredItems = products.filter((product) =>
+          product.name.toLowerCase().includes(searchTerm.toLowerCase())
+        );
+    
+        setFilteredProducts(filteredItems);
+      }
 
     // Функция удаления продукта
-    async function removeProducts(id) {
-        // const response = await supabase
-        //     .from('products')
-        //     .delete()
-        //     .eq('id', id)
+//     async function removeProducts(id) {
+//         // const response = await supabase
+//         //     .from('products')
+//         //     .delete()
+//         //     .eq('id', id)
 
-        // if (response.status === 204) {
-        //     setProducts(products.filter(p => p.id != id))
-        // } else {alert('Нихуя не получилось')}
-}
+//         // if (response.status === 204) {
+//         //     setProducts(products.filter(p => p.id != id))
+//         // } else {alert('Нихуя не получилось')}
+// }
 
 
     return (
         <div className={styles.container}>
             <h1 className={styles.title}>SKLAD</h1>
-            <SearchField />
+            <SearchField handleInputChange={handleInputChange} searchItem={searchItem} />
             <Tabs />
-            {products.map(product => (
-                <ProductItem product={product} key={product.id} quantity={product.quantity} removeProducts={removeProducts} />
+            {filteredProducts.map(product => (
+                <ProductItem product={product} key={product.id} quantity={product.quantity} />
             ))}
         </div>
     )
